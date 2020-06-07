e66e271b4002eeecdcf79f561aa9850e92a88d55
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-07 21:53:38 +0800

    初始化代码

---
+++ day02_servlet/src/com/cc1021/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.cc1021.dao;
+
+import com.cc1021.domain.User;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+
+/**
+ *  操作数据库中 User 的类
+ */
+public class UserDao {
+    /**
+     * 登录方法
+     * @param loginUser 只有用户名和密码
+     * @return user包含用户全部数据
+     */
+    public User login(User loginUser){
+        return null;
+    }
+}
---
+++ day02_servlet/src/com/cc1021/util/JDBCUtils.java
@@ -0,0 +1,47 @@
+package com.cc1021.util;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *  JDBC工具类 使用 Druid 连接池
+ */
+public class JDBCUtils {
+
+    private static DataSource ds;
+
+    static {
+        try {
+            //1、加载配置文件
+            Properties pro = new Properties();
+            //使用 ClassLoader 加载配置文件，获取字节输入流
+            InputStream is = JDBCUtils.class.getClassLoader().getResourceAsStream("druid.properties");
+            pro.load(is);
+
+            //2、初始化连接池对象
+            ds = DruidDataSourceFactory.createDataSource(pro);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取连接池对象
+     */
+    public static DataSource getDataSource() {
+        return ds;
+    }
+
+    /**
+     * 获取连接 Connection 对象
+     */
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
