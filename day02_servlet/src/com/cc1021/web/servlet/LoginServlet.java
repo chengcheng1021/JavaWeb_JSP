d4f7c07f0f2ef382c300775d33a68325033d3e4f
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-08 17:28:02 +0800

    beanutils优化代码

--- day02_servlet/src/com/cc1021/web/servlet/LoginServlet.java
+++ day02_servlet/src/com/cc1021/web/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package com.cc1021.web.servlet;
 
 import com.cc1021.dao.UserDao;
 import com.cc1021.domain.User;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
@@ -17,14 +20,27 @@ public class LoginServlet extends HttpServlet {
         //1、设置编码
         request.setCharacterEncoding("utf-8");
 
-        //2、获取请求参数
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+//        //2、获取请求参数
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        //3、封装user对象
+//        User loginUser = new User();
+//        loginUser.setUsername(username);
+//        loginUser.setPassword(password);
 
-        //3、封装user对象
+        //2、获取所有请求参数
+        Map<String, String[]> map = request.getParameterMap();
+        //3、创建 User 对象
         User loginUser = new User();
-        loginUser.setUsername(username);
-        loginUser.setPassword(password);
+        //3.2、使用 BeanUtils 封装
+        try {
+            BeanUtils.populate(loginUser, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
         //4、调用UserDao的login方法
         UserDao dao = new UserDao();
--- day02_servlet/src/com/cc1021/web/servlet/SuccessServlet.java
+++ day02_servlet/src/com/cc1021/web/servlet/SuccessServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "/successServlet")
+@WebServlet(urlPatterns = "/successServlet")
 public class SuccessServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
