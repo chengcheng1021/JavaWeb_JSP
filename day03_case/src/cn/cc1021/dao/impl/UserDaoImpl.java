14e9caea2030fcfe4ca2ac74ae1960cc4fa25f98
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-08 20:58:31 +0800

    用户信息代码完善

--- day03_case/src/cn/cc1021/dao/impl/UserDaoImpl.java
+++ day03_case/src/cn/cc1021/dao/impl/UserDaoImpl.java
@@ -2,13 +2,21 @@ package cn.cc1021.dao.impl;
 
 import cn.cc1021.dao.UserDao;
 import cn.cc1021.domain.User;
+import cn.cc1021.util.JDBCUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
+
+    private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
     @Override
     public List<User> findAll() {
         //使用JDBC操作数据库...
-        return null;
+        //1、定义 sql
+        String sql = "select * from users";
+        List<User> users = template.query(sql, new BeanPropertyRowMapper<User>(User.class));
+        return users;
     }
 }
--- day03_case/src/cn/cc1021/service/impl/UserServiceImpl.java
+++ day03_case/src/cn/cc1021/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package cn.cc1021.service.impl;
 
+import cn.cc1021.dao.UserDao;
+import cn.cc1021.dao.impl.UserDaoImpl;
 import cn.cc1021.domain.User;
 import cn.cc1021.service.UserService;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private UserDao dao = new UserDaoImpl();
     /**
      * 查询所有用户信息
      *
@@ -14,6 +17,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         //调用Dao完成查询
-        return null;
+        return dao.findAll();
     }
 }
