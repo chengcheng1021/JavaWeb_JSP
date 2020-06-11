04eca2f52f7919b21add533e6d7a9e2191973c14
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-11 22:24:07 +0800

    注解监听器

--- day04_filter&listener/src/cn/cc1021/listener/ContextLoaderListener.java
+++ day04_filter&listener/src/cn/cc1021/listener/ContextLoaderListener.java
@@ -1,8 +1,13 @@
 package cn.cc1021.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+@WebListener
 public class ContextLoaderListener implements ServletContextListener {
     /**
      * 监听 ServletContext 对象创建的。ServletContext 对象服务器启动后自动创建。
@@ -12,6 +17,24 @@ public class ContextLoaderListener implements ServletContextListener {
      */
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        //加载资源路径
+        //1、获取 ServletContext 对象
+        ServletContext servletContext = sce.getServletContext();
+
+        //2、加载资源文件
+        String contextConfigLocation = servletContext.getInitParameter("contextConfigLocation");
+
+        //3、获取真实路径
+        String realPath = servletContext.getRealPath(contextConfigLocation);
+
+        //4、加载进内存
+        try {
+            FileInputStream fis = new FileInputStream(realPath);
+            System.out.println(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ServletContext 对象创建了。。。");
     }
 
