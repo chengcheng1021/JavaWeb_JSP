7de8cf9881810adf3890ad8fdf3ef94778edeaa0
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-11 09:38:31 +0800

    测试类

--- day03_case/src/cn/cc1021/test/UserDaoTest.java
+++ day03_case/src/cn/cc1021/test/UserDaoTest.java
@@ -5,7 +5,10 @@ import cn.cc1021.dao.impl.UserDaoImpl;
 import cn.cc1021.domain.User;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class UserDaoTest {
 
@@ -26,5 +29,12 @@ public class UserDaoTest {
         List<User> all = dao.findAll();
         System.out.println(all);
 
+    }
+
+    @Test
+    public void testRandomStudy(){
+        List<String> strings = new ArrayList<String>(Arrays.asList("golang", "php", "js", "nginx", "mysql", "redis"));
+        Random random = new Random();
+        System.out.println(strings.get(random.nextInt(strings.size())));
     }
 }
