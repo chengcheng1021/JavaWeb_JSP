063bc2d0eccbf66c1e926c6fcc8935dd57c85fd4
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-15 21:35:43 +0800

    删除引用mybatis,自定义接口和方法

---
+++ day01_mybatis_design/src/main/java/com/cc/sqlsession/SqlSession.java
@@ -0,0 +1,21 @@
+package com.cc.sqlsession;
+
+/**
+ * 自定义 Mybatis 中和数据库交互的核心类
+ * 它里面可以创建 dao 接口的代理对象
+ */
+public interface SqlSession {
+
+    /**
+     * 根据参数创建一个代理对象
+     * @param daoInterfaceClass
+     * @param <T>
+     * @return
+     */
+    <T> T getMapper(Class<T> daoInterfaceClass);
+
+    /**
+     * 释放资源
+     */
+    void close();
+}
---
+++ day01_mybatis_design/src/main/java/com/cc/sqlsession/SqlSessionFactoryBuilder.java
@@ -0,0 +1,18 @@
+package com.cc.sqlsession;
+
+import java.io.InputStream;
+
+/**
+ * 用于创建一个 SqlSessionFactory 对象
+ */
+public class SqlSessionFactoryBuilder {
+
+    /**
+     * 根据参数的字节输入流来构建一个 SqlSessionFactory 工厂
+     * @param config
+     * @return
+     */
+    public SqlSessionFactory build(InputStream config) {
+        return null;
+    }
+}
