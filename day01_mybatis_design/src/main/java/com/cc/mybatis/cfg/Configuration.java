8fc6863dbd4ddf1cd26292f87f550facc7dd8a3a
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-15 22:48:58 +0800

    实现基于xml的查询所有操作

--- day01_mybatis_design/src/main/java/com/cc/mybatis/cfg/Configuration.java
+++ day01_mybatis_design/src/main/java/com/cc/mybatis/cfg/Configuration.java
@@ -1,5 +1,6 @@
 package com.cc.mybatis.cfg;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -12,7 +13,7 @@ public class Configuration {
     private String username;
     private String password;
 
-    private Map<String, Mapper> mappers;
+    private Map<String, Mapper> mappers = new HashMap<String, Mapper>();
 
     public Map<String, Mapper> getMappers() {
         return mappers;
--- day01_mybatis_design/src/main/java/com/cc/mybatis/sqlsession/defaults/DefaultSqlSession.java
+++ day01_mybatis_design/src/main/java/com/cc/mybatis/sqlsession/defaults/DefaultSqlSession.java
@@ -2,6 +2,12 @@ package com.cc.mybatis.sqlsession.defaults;
 
 import com.cc.mybatis.cfg.Configuration;
 import com.cc.mybatis.sqlsession.SqlSession;
+import com.cc.mybatis.sqlsession.proxy.MapperProxy;
+import com.cc.mybatis.utils.DataSourceUtil;
+
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * SqlSession 接口的实现类
@@ -9,9 +15,11 @@ import com.cc.mybatis.sqlsession.SqlSession;
 public class DefaultSqlSession implements SqlSession {
 
     private Configuration cfg;
+    private Connection connection;
 
     public DefaultSqlSession(Configuration cfg){
         this.cfg = cfg;
+        connection = DataSourceUtil.getConnection(cfg);
     }
 
     /**
@@ -21,13 +29,20 @@ public class DefaultSqlSession implements SqlSession {
      * @return
      */
     public <T> T getMapper(Class<T> daoInterfaceClass) {
-        return null;
+        return (T) Proxy.newProxyInstance(daoInterfaceClass.getClassLoader(),
+                new Class[]{daoInterfaceClass}, new MapperProxy(cfg.getMappers(), connection));
     }
 
     /**
      * 用于释放资源
      */
     public void close() {
-
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 }
