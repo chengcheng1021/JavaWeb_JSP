cd1931b557e43928c509319581801926dec2fdfd
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-17 08:36:14 +0800

    实现新增用户功能

--- day02_mybatis_CURD/src/main/java/com/cc/dao/IUserDao.java
+++ day02_mybatis_CURD/src/main/java/com/cc/dao/IUserDao.java
@@ -14,4 +14,10 @@ public interface IUserDao {
      * @return
      */
     List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    void saveUser(User user);
 }
--- day02_mybatis_CURD/src/test/java/com/cc/test/MybatisTest.java
+++ day02_mybatis_CURD/src/test/java/com/cc/test/MybatisTest.java
@@ -6,10 +6,13 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,31 +20,59 @@ import java.util.List;
  */
 public class MybatisTest {
 
-    /**
-     * 测试查询所有
-     */
-    @Test
-    public void testFindAll() throws IOException {
+    private InputStream is;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+    @Before//用于在测试方法之前执行
+    public void init() throws IOException {
         //1、读取配置文件，生成字节输入流
-        InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
+        is = Resources.getResourceAsStream("SqlMapConfig.xml");
 
         //2、获取SqlSessionFactory
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
 
         //3、获取SqlSession对象
-        SqlSession sqlSession = factory.openSession();
+        sqlSession = factory.openSession();
 
         //4、获取dao的代理对象
-        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
 
+    @After//用于在测试方法之后执行
+    public void destory() throws IOException {
+        //6、释放资源
+        sqlSession.close();
+        is.close();
+    }
+
+    /**
+     * 测试查询所有
+     */
+    @Test
+    public void testFindAll() throws IOException {
         //5、执行查询所有方法
         List<User> users = userDao.findAll();
         for (User user : users) {
             System.out.println(user);
         }
+    }
 
-        //6、释放资源
-        sqlSession.close();
-        is.close();
+    /**
+     * 测试保存操作
+     */
+    @Test
+    public void testSaveUser() {
+        User user = new User();
+        user.setSex("男");
+        user.setAddress("南京市");
+        user.setUsername("cheng");
+        user.setBirthday(new Date());
+
+        //5、执行保存方法
+        userDao.saveUser(user);
+
+        //提交事务
+        sqlSession.commit();
     }
 }
